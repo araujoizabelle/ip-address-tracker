2e899f74cd0bcf833008096fdf0801104e7b19e6
Izabelle Derrihu <izabelle.derrihu@idwall.co>
2024-01-11 20:16:16 -0300

    css much better now

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,8 @@ interface location {
 /**
  * TODO: 
  * 1- create custom Hook to fetch the data on load page using some default ip
- * 2- css 
+ * 2- Marker automatically change on search
+ * 3- skeleton screen to load the content  
  */
 
 const apiKey = process.env.apikey;
@@ -40,49 +41,50 @@ function App() {
     } catch (error) {
       console.error('erro na request', error)
     }
-   
   }
 
   return (
     <div className='container'>
+      
       <div className='container-search'>
-        <input type='text' value={searchValue} onChange={(e) => {setSearchValue(e.target.value)}}/>
-        <button onClick={handleSearch}> &gt; </button>
+        <h1 className='title'>IP Address Tracker</h1>
+        <input type='text' placeholder='Search for any IP address or domain' value={searchValue} onChange={(e) => {setSearchValue(e.target.value)}}/>
+        <button onClick={handleSearch}> <img src='src/assets/images/icon-arrow.svg'/> </button>
       </div>
 
       {locationData && 
         <div className='container-info'>
           <div className='container-info--item'>
-            <div>Ip Address</div>
-            <div>{locationData.ip}</div>
+            <span className='info-item--title'>Ip Address</span>
+            <p>{locationData.ip}</p>
           </div>
           <div className='container-info--item'>
-            <div>Location</div>
-            <div>
+            <span className='info-item--title'>Location</span>
+            <p>
             {locationData?.city}, {locationData?.region} - {locationData?.postalCode}
-            </div>
+            </p>
           </div>
           <div className='container-info--item'>
-            <div>Timezone</div>
-            <div>{locationData?.timezone}</div>
+            <span className='info-item--title'>Timezone</span>
+            <p>UTC {locationData?.timezone}</p>
           </div>
           <div className='container-info--item'>
-            <div>ISP</div>
-            <div>{locationData?.isp}</div>
+            <span className='info-item--title'>ISP</span>
+            <p>{locationData?.isp}</p>
           </div>
         </div>
       }
 
       {locationData && 
         <div className='container-map'>
-          <MapContainer style={{height: '300px' }} center={[locationData.lat, locationData.lng]} zoom={13} scrollWheelZoom={false}>
+          <MapContainer center={[locationData.lat, locationData.lng]} zoom={13} scrollWheelZoom={false}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <Marker position={[locationData.lat, locationData.lng]}>
               <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
+                Double click to zoom in
               </Popup>
             </Marker>
           </MapContainer>
